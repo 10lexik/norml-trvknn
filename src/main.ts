b94f3a5b7331bc23302199e94c590e737ce8c9a3
Gwennael Larcher <glarcher@veepee.com>
2026-03-10 16:29:22 +0100

    chore: remove stale inline comments in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 // src/main.ts
-import './assets/scss/main.scss' // <--- INDISPENSABLE pour le fond beige et la police
+import './assets/scss/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
-import i18n from './plugins/i18n' // ou ton chemin correct
+import i18n from './plugins/i18n'
 
 const app = createApp(App)
 app.use(i18n)
